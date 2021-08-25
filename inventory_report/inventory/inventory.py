a1c6f9addb5d0b6131411a0070399824ed54c24c
Gabriel-Goncalves <gabriel_98gm@hotmail.com>
2021-08-25 16:48:43 -0300

    adicionando req 3 e 4

---
+++ inventory_report/inventory/inventory.py
@@ -0,0 +1,25 @@
+import json
+import csv
+from inventory_report.reports.complete_report import CompleteReport
+from inventory_report.reports.simple_report import SimpleReport
+
+
+class Inventory:
+    @classmethod
+    def get_list_by_file_path(cls, path):
+        with open(path, mode="r") as csv_or_json_content:
+            if path.endswith(".json"):
+                return json.load(csv_or_json_content)
+            elif path.endswith(".csv"):
+                return list(csv.DictReader(csv_or_json_content))
+            else:
+                return None
+
+    @classmethod
+    def import_data(cls, path, report_type):
+        readed_content = cls.get_list_by_file_path(path)
+        if report_type == "simples":
+            return SimpleReport.generate(readed_content)
+        elif report_type == "completo":
+            return CompleteReport.generate(readed_content)
+        return None
